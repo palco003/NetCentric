a6a3beb497578468c65843822a2f323a97dbbd29
Pablo Alcocer <palco003@fiu.edu>
2017-08-21 22:30:53 -0400

    Add first exercise file

---
+++ Python/HardWay/ex1.py
@@ -0,0 +1,11 @@
+"""
+This first exercise is extremely easy and is meant to introduce the reader to the idea of print statements and running a python script in the terminal.
+"""
+print "Hello world!"
+print "Hello again"
+print "I like typing this."
+print "This is fun."
+print "Yay! Printing."
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+
